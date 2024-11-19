98608d7a225e633ff09884528808b976167da340
Nadir Hossain <93004681+ohnadir@users.noreply.github.com>
2024-11-19 09:59:35 +0000

    :refactor :recycle: refactor code

--- Config.jsx
+++ Config.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseAxios = axios.create({
     
-    baseURL: "http://103.161.9.43:3000",
+    baseURL: "http://146.190.126.8:5000",
     timeout: 10000,
     headers: {'X-Custom-Header': 'foobar'}
   });
